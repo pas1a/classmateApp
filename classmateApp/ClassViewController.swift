1830cff75dc7400e52d4904e3d982fe0eb2b20fe
John <john.gariepy25@d155.org>
2024-10-08 08:03:58 -0500

    FINAL VERISON

--- classmateApp/ClassViewController.swift
+++ classmateApp/ClassViewController.swift
@@ -12,20 +12,27 @@ class ClassViewController: UIViewController {
     @IBOutlet weak var classTextView: UITextView!
     
     var text = ""
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        classTextView.text = GlobalClass.clasHolder[0].toString()
         // Do any additional setup after loading the view.
         
         
     }
     @IBAction func refrershButton(_ sender: UIButton) {
-       
+        if count < GlobalClass.clasHolder.count-1{
+            count += 1
+            classTextView.text = GlobalClass.clasHolder[count].toString()
+        }
 
     }
     @IBAction func nextButton(_ sender: UIButton) {
-        
+        if count != 0{
+            count -= 1
+            classTextView.text = GlobalClass.clasHolder[count].toString()
+        }
         
         
     }
